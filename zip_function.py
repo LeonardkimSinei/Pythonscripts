9eb1727fc3099fe9426da5d837b111b82ff4e770
LeonardkimSinei <leonard97kimutai@gmail.com>
2024-02-16 23:40:42 +0300

    Zip function usage
    Creating a dictionary using zip with two lists

---
+++ zip_function.py
@@ -0,0 +1,11 @@
+my_debts ={"Alfrida":100, "Loice":250, "Mercy": 50, "Wesley": 300}
+new_deni = list(my_debts.values())
+
+my_list = [1,2,3,4,5]
+mylisttwo =["a","b","c","d","e"]
+
+for key ,value in zip(my_list,mylisttwo):
+    print (f'{key}:{value}')
+
+
+
